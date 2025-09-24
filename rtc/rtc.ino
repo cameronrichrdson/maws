d4422b0dbbca812e9052a7d9dc8ea994da1fa5ce
Cameron Richardson <113459407+cameronrichrdson@users.noreply.github.com>
2025-09-24 12:56:26 -1000

    added support for temperature sensor logging
    measures and logs date,time, tempeature, conductivity

--- rtc/rtc.ino
+++ rtc/rtc.ino
@@ -3,16 +3,23 @@
 #include <SD.h>
 #include <SPI.h>
 #include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-// === Atlas Scientific RTD Setup ===
+// === Temperature sensor (DS18B20) ===
+#define ONE_WIRE_BUS 8
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+float temperature;
+
+// === Atlas Scientific EC Setup ===
 #define RX 2
-#define TX 5
+#define TX 7
 SoftwareSerial myserial(RX, TX);
-
 String inputstring = "";
 String sensorstring = "";
 boolean sensor_string_complete = false;
-float temperature = NAN;
+float conductivity = NAN;
 
 // === RTC + SD Setup ===
 DS3231 myRTC;
@@ -31,7 +38,7 @@ String twoDigits(int number) {
 void setup() {
   Serial.begin(9600);
   myserial.begin(9600);
-
+  sensors.begin();
   inputstring.reserve(10);
   sensorstring.reserve(30);
 
@@ -44,11 +51,14 @@ void setup() {
     while (1);
   }
   Serial.println("SD card initialized.");
-
   Serial.println("Commands: STOP, START, RESET, SETTIME dd/mm/yyyy hh:mm:ss");
 }
 
 void loop() {
+  // === Read DS18B20 ===
+  sensors.requestTemperatures();
+  temperature = sensors.getTempCByIndex(0);
+
   // === Handle Serial Commands ===
   if (Serial.available()) {
     String cmd = Serial.readStringUntil('\n');
@@ -67,12 +77,12 @@ void loop() {
     else if (cmdUpper == "RESET") {
       if (SD.exists("datalog.txt")) SD.remove("datalog.txt");
       Serial.println("Log file reset.");
-      fileInitialized = false; // allow header to be re-added
+      fileInitialized = false;
     } 
     else if (cmdUpper.startsWith("SETTIME")) {
       int d, m, y, h, mi, s;
       if (sscanf(cmd.c_str(), "SETTIME %d/%d/%d %d:%d:%d", &d, &m, &y, &h, &mi, &s) == 6) {
-        myRTC.setClockMode(false); // 24h
+        myRTC.setClockMode(false);
         myRTC.setYear(y - 2000);
         myRTC.setMonth(m);
         myRTC.setDate(d);
@@ -86,16 +96,18 @@ void loop() {
     }
   }
 
-  // === Handle Atlas Scientific Sensor Input ===
+  // === Handle Atlas Scientific EC Input ===
   if (myserial.available() > 0) {
     char inchar = (char)myserial.read();
     sensorstring += inchar;
     if (inchar == '\r') sensor_string_complete = true;
   }
 
-  if (sensor_string_complete) {
-    if (isdigit(sensorstring[0])) {
-      temperature = sensorstring.toFloat();
+  if (sensor_string_complete == true) {
+    if (isdigit(sensorstring[0]) == false) {
+      Serial.println(sensorstring);   // not numeric, print raw
+    } else {
+      parseEC();                      // parse conductivity
     }
     sensorstring = "";
     sensor_string_complete = false;
@@ -114,15 +126,12 @@ void loop() {
                    String(year + 2000) + "," +
                    twoDigits(hour) + ":" +
                    twoDigits(minute) + ":" +
-                   twoDigits(second) + ",";
+                   twoDigits(second);
 
   // === Build log string ===
-  String logStr = timeStr;
-  if (!isnan(temperature)) {
-    logStr +=  String(temperature, 3);
-  } else {
-    logStr += "N/A";
-  }
+  String logStr = timeStr + "," +
+                  String(temperature, 2) + "," +
+                  (isnan(conductivity) ? "nan" : String(conductivity, 3));
 
   Serial.println(logStr);
 
@@ -131,14 +140,14 @@ void loop() {
     if (!SD.exists("datalog.txt")) {
       File dataFile = SD.open("datalog.txt", FILE_WRITE);
       if (dataFile) {
-        dataFile.println("Date,Time,Temperature");
+        dataFile.println("Date,Time,Temperature,Conductivity");
         dataFile.close();
         Serial.println("Log file created with header.");
       } else {
         Serial.println("Error creating datalog.txt");
       }
     }
-    fileInitialized = true; // prevent running every loop
+    fileInitialized = true;
   }
 
   // === Write to SD ===
@@ -153,4 +162,15 @@ void loop() {
   }
 
   delay(1000);
+}
+
+// === Parse EC string from Atlas probe ===
+void parseEC() {
+  char sensorstring_array[30];
+  sensorstring.toCharArray(sensorstring_array, 30);
+
+  char *val = strtok(sensorstring_array, ","); // first token = EC
+  if (val != NULL) {
+    conductivity = atof(val); // convert to float
+  }
 }
